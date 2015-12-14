ac721b846f306ecbc2cdea7e44999ed5d1ce56ad
Lewis Cowper <lewis.cowper@googlemail.com>
2015-12-14 21:27:43 +0100

    fix(tests): catch errors as thrown

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ test ('confidence check with all styles', function (t) {
     html: '<div id="test1" style="font-family: Helvetica; font-size: 16px; font-weight: 400; font-style: normal;"></div>',
     scripts: [],
     done: function (err, window) {
+      if (err) {
+        throw err;
+      }
+
       global.window = window;
 
       var document = window.document;
@@ -24,6 +28,10 @@ test ('confidence check with a missing style', function (t) {
     html: '<div id="test1" style="font-family: Helvetica; font-weight: 400; font-style: normal;"></div>',
     scripts: [],
     done: function (err, window) {
+      if (err) {
+        throw err;
+      }
+      
       global.window = window;
 
       var document = window.document;
